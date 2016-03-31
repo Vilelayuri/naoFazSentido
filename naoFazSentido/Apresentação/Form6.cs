aa23d7eaa3031c25365f9eefc78f2ec661c59f3e
Vilelayuri <vilela.yurisouza@gmail.com>
2016-03-31 20:52:16 -0300

    Update Form6.cs

--- naoFazSentido/Apresentação/Form6.cs
+++ naoFazSentido/Apresentação/Form6.cs
@@ -8,54 +8,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using naoFazSentido.Negocio;
-using naoFazSentido.ObjetoDeTransferencia;
 
 namespace naoFazSentido.Apresentação
 {
-    public partial class Form5 : Form
+    public partial class Form6 : Form
     {
-        public Form5()
+        public Form6()
         {
             InitializeComponent();
-            comboBox1.Items.Add("Etanol");
-            comboBox1.Items.Add("Gasolina");
-            comboBox1.Items.Add("Flex");
         }
-        DTOCarro car = new DTOCarro();
-        Negocios control = new Negocios(); 
-        private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+        Negocios negocio = new Negocios();
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            car.Combustivel = comboBox1.Text;
-            car.Cor = Mtbcor.Text;
-            car.Marca = mtbMarca.Text;
-            car.Modelo = mtbModelo.Text;
-            car.Placa = MtbPlaca.Text;
-            car.Quilometragem = MtbKM.Text;
-            car.Renavam = mtbRenavam.Text;
-            control.Insericar(car);
-            
-
-        }
-
-        private void excluirToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            control.ExcluirCar(MtbPlaca.Text);
-        }
-
-        private void cancelarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-           comboBox1.Text = "";
-           Mtbcor.Text = "";
-           mtbMarca.Text = "";
-           mtbModelo.Text = "";
-           MtbPlaca.Text = "";
-           MtbKM.Text = "";
-          mtbRenavam.Text = "";
-        }
-
-        private void procurarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
+            if (comboBox1.Text == "Carros")
+            {
+              dataGridView1.DataSource = negocio.SelecionarVeiculo();
+            }
+            else if (comboBox1.Text == "Motorista")
+            {
+                dataGridView1.DataSource = negocio.SelecionaTodosMotorista();
+            }
+            else if (comboBox1.Text == "Vistoria")
+            {
+                dataGridView1.DataSource = negocio.SelecionaTodasRevisoes();
+            }
         }
     }
 }
